7b161e014d88825689e32d2a169efb28df1ee7e3
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-07-02 06:36:28 +0700

    Add files via upload

---
+++ animasi.h
@@ -0,0 +1,50 @@
+/*Saya Muhammad 'Azmi Salam mengerjakan evaluasi Tugas Masa
+Depan dalam mata kuliah Dasar-dasar Pemrograman untuk
+keberkahanNya maka saya tidak melakukan kecurangan seperti
+yang telah dispesifikasikan. Aamiin.*/
+
+#include <unistd.h> // deklarasi library yang akan digunakan
+#include <time.h>   // deklarasi library yang akan digunakan
+#include <conio.h>   // deklarasi library yang akan digunakan
+
+// Prosedur Buku Panduan
+void panduan();
+
+// Prosedur Help Insert
+void help_insert();
+void help_insert_kasus();
+void help_insert_sin();
+void help_insert_hakim();
+void help_insert_gedung();
+
+// Prosedur Help Update
+void help_update();
+void help_update_kasus();
+void help_update_sin();
+void help_update_hakim();
+void help_update_gedung();
+
+// Prosedur Help Delete
+void help_delete();
+void help_delete_kasus();
+void help_delete_sin();
+void help_delete_hakim();
+void help_delete_gedung();
+
+// Prosedur Help Show
+void help_show();
+void help_show_kasus();
+void help_show_sin();
+void help_show_hakim();
+void help_show_gedung();
+
+// Prosedur Help Join
+void help_join();
+void help_join_kasus();
+
+// Prosedur Help MultiQuery
+void help_multiquery();
+
+// Prosedur Animasi Intro dan Outro
+void intro();
+void outro();
